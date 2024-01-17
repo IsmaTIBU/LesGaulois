26efe0c49d13d39601463261685da8a0442d1c08
Ismael Monge rouchdi <mns3510a@ensfsi.onmicrosoft.com>
2024-01-17 09:57:15 +0100

    Exo 9

---
+++ src/personnages/Romain.java
@@ -0,0 +1,32 @@
+package personnages;
+
+public class Romain {
+	private String nom;
+	private int force;
+	
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public int recevoirCoup(int forceCoup) {
+		force-=forceCoup;
+		if(forceCoup<0) {
+			force=0;
+		}
+		return force;
+	}
+	
+	public void parler(String phrase) {
+		System.out.println("Le romain "+getNom()+": '"+phrase+"'");
+		if(force==0) {
+			System.out.println("Le romain "+getNom()+": "+"J'abandonne");
+		}else {
+			System.out.println("Le romain "+getNom()+": "+"Aîe !");
+		}
+	}
+}
--- src/test_fontionnel/TestGaulois.java
+++ src/test_fontionnel/TestGaulois.java
@@ -1,10 +1,17 @@
 package test_fontionnel;
 import personnages.Gaulois;
+import personnages.Romain;
 
 public class TestGaulois {
 	public static void main(String[] args) {
 		Gaulois asterix=new Gaulois("Asterix",8);
 		System.out.println(asterix.getNom());
 		asterix.parler("Bonjour à tous");
+		Romain romain=new Romain("Minus",6);
+		romain.parler("UN GAU… UN GAUGAU…");
+		for(i=0;i<2;i++) {
+			romain.recevoirCoup(3);
+			romain.parler
+		}
 	}
 }
